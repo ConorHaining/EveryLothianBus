3fe845490d823c142cfc0fcca838fb2fd36cd716
Conor Haining <conor.haining@gmail.com>
2020-01-25 20:30:59 +0000

    feat: creation of the polling service

--- Services/PollingService.cs
+++ Services/PollingService.cs
@@ -1,28 +1,53 @@
 using System;
+using System.Net.Http;
+using System.Timers;
 
 public class PollingService : IPollingService
 {
+    private readonly HttpClient _httpClient;
     private readonly Uri address;
-    private readonly TimeSpan pollInterval;
+    private Timer Timer;
 
-    public PollingService(Uri address, TimeSpan pollInterval)
+    public PollingService(HttpClient _httpClient, Uri address, TimeSpan pollInterval)
     {
+        this._httpClient = _httpClient;
         this.address = address;
-        this.pollInterval = pollInterval;
+
+        Timer = new Timer(pollInterval.TotalMilliseconds);
+        Timer.Elapsed += Poll;
+        Timer.AutoReset = true;
+        Timer.Enabled = true;
     }
 
     public PollingStatus Start()
     {
-        throw new NotImplementedException();
+        Timer.Start();
+        Timer.Enabled = true;
+        return PollingStatus.Started;
     }
 
     public PollingStatus Status()
     {
-        throw new NotImplementedException();
+        if (Timer.Enabled)
+        {
+            return PollingStatus.Running;
+        }
+        else
+        {
+            return PollingStatus.Stopped;
+        }
     }
 
     public PollingStatus Stop()
     {
-        throw new NotImplementedException();
+        Timer.Stop();
+        Timer.Enabled = false;
+        return PollingStatus.Stopped;
+    }
+
+    private void Poll(Object source, ElapsedEventArgs e)
+    {
+        Console.WriteLine("The Elapsed event was raised at {0}", e.SignalTime);
+        var result = _httpClient.GetAsync(address);
     }
 }
