4b7c1fc6c1e6442036a2f7029c8d4d1f07904328
Conor Haining <conor.haining@gmail.com>
2020-02-20 22:08:53 +0000

    remove model

--- Models/VehicleLocationsResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace EveryBus.Models
-{
-    public class VehicleLocationsResponse
-    {
-        public double last_updated { get; set; }
-        public VehicleLocation[] vehicles { get; set; }
-    }
-
-}
