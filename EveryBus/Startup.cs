97b21afce64a245d203e55ff665464b69c890ac1
Conor Haining <conor.haining@gmail.com>
2020-03-14 12:31:49 +0000

    style: extract latest locations to a service

--- EveryBus/Startup.cs
+++ EveryBus/Startup.cs
@@ -27,7 +27,9 @@ namespace EveryBus
         {
             services.AddRazorPages();
 
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(options =>
+            {
+            });
             services.AddHttpClient("polling", client => { })
             .AddTransientHttpErrorPolicy(builder => builder.CircuitBreakerAsync(
                handledEventsAllowedBeforeBreaking: 3,
@@ -38,10 +40,11 @@ namespace EveryBus
             services.AddSingleton<IPollingService, PollingService>();
             services.AddSingleton<IRouteService, RouteService>();
             services.AddDbContext<BusContext>(
-                ops => ops.UseMySql(@"server=db;user=dbuser;password=dbuserpassword;database=buses;"),
-                // ops => ops.UseMySql(@"server=localhost;port=1234;user=dbuser;password=dbuserpassword;database=buses;"),
+                // ops => ops.UseMySql(@"server=db;user=dbuser;password=dbuserpassword;database=buses;"),
+                ops => ops.UseMySql(@"server=localhost;port=1234;user=dbuser;password=dbuserpassword;database=buses;"),
                 ServiceLifetime.Singleton, ServiceLifetime.Singleton);
             services.AddSingleton<IObserver<VehicleLocation[]>, PersistLocations>();
+            services.AddTransient<IVehicleLocationsService, VehicleLocationsService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
